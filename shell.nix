7d2d2e16a0a58335d340cff573dea7a7da607859
steve-chavez <stevechavezast@gmail.com>
2020-09-22 13:27:42 -0500

    Upgrade k6 version to 0.27

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
   nixpkgs = builtins.fetchTarball {
     name = "nixos-20.03";
-    url = "https://github.com/nixos/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz";
-    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+    url = "https://github.com/nixos/nixpkgs/archive/7bc3a08d3a4c700b53a3b27f5acd149f24b931ec.tar.gz";
+    sha256 = "1kiz37052zsgvw7a378zg08mpbi1wk8dkgm5j6dy0x4mxvcg8ws3";
   };
   pkgs = import nixpkgs {};
 in
