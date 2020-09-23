37cf45e562f8e592f7ef00700c2cab926ccad609
steve-chavez <stevechavezast@gmail.com>
2020-09-23 18:16:34 -0500

    Add k6 writes load scripts

---
+++ writes/singleObjectWColumns.js
@@ -0,0 +1,51 @@
+import { Rate, Gauge } from "k6/metrics";
+import { check, group, sleep } from 'k6';
+import http from 'k6/http';
+
+const URL = "http://" + __ENV.URL;
+
+export let options = {
+  discardResponseBodies: true,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1200, //setting 1100 leads to dropped iterations
+      timeUnit: '1s',
+      duration: '30s',
+      preAllocatedVUs: 100,
+      maxVUs: 600,
+    }
+  },
+  thresholds: {
+    'failed requests': ['rate<0.1'],
+    'http_req_duration': ['p(95)<1000']
+  }
+};
+
+const myFailRate = new Rate('failed requests');
+
+export default function() {
+  let body = JSON.stringify({
+    first_name:  'Virtual ' + __ITER
+  , last_name:   'User '    + __ITER
+  , title:       'Load Tester'
+  , reports_to:  1
+  , birth_date:  '1920-01-01'
+  , hire_date:   '2020-01-01'
+  , address:     '666 10 Street SW'
+  , city:        'Calgary'
+  , state:       'AB'
+  , country:     'Canada'
+  , postal_code: 'T2P 5G'
+  , phone:       '(403) 246-9887'
+  , fax:         '+1 (403) 246-9899'
+  , email:       'vu' + __ITER + '@chinookcorp.com'
+  });
+  let res = http.post(URL + "/employee?columns=" + Object.keys(body).reduce((x, y) => x + "," + y),
+    body, {headers: { 'Content-Type': 'application/json' }});
+  myFailRate.add(res.status !== 201);
+}
+
+//export function teardown(data) {
+  //http.del(URL + "/employee?title=eq.Load%20Tester", {}, {headers: { 'Prefer': 'count=exact' }});
+//}
