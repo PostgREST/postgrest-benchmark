c821f4b3d22438bd22c85ac9ee7d1b4aa92ad39d
steve-chavez <stevechavezast@gmail.com>
2020-10-29 13:08:38 -0500

    Add RPC load tests

---
+++ k6/RPCSimple.js
@@ -0,0 +1,49 @@
+import { Rate } from "k6/metrics";
+import { check, group, sleep } from 'k6';
+import http from 'k6/http';
+
+const URL = "http://" + __ENV.HOST;
+
+const RATE = (function(){
+  if(__ENV.VERSION == 'v701'){
+    switch(__ENV.HOST){
+      case 'c5xlarge': return 2400;
+      case 't3axlarge':  return 1600;
+      case 't3anano':  return 1600;
+      default:         return 1500;
+    }
+  }
+  else switch(__ENV.HOST){
+      case 'c5xlarge': return 3000;
+      case 't3axlarge':  return 2200;
+      case 't3anano':  return 2000;
+      default:         return 1000;
+    }
+})();
+
+export let options = {
+  discardResponseBodies: true,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: RATE,
+      timeUnit: '1s',
+      duration: '30s',
+      preAllocatedVUs: 100,
+      maxVUs: 600,
+    }
+  },
+  thresholds: {
+    'failed requests': ['rate<0.1'],
+    'http_req_duration': ['p(95)<1000']
+  }
+};
+
+const myFailRate = new Rate('failed requests');
+
+export default function() {
+  //let res = http.get(URL + "/rpc/add_them1?a=1&b=2&c=3&d=4&e=5");
+  let res = http.get(URL + "/rpc/add_them2?a=1&b=2&c=3&d=4&e=5&v=0");
+  //let res = http.get(URL + "/rpc/add_them3?v=1&v=2&v=3&v=4&v=5");
+  myFailRate.add(res.status !== 200);
+}
